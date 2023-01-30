0183df4e8f9b218e8e6c8918686f3d9149bf68ac
WandirPereira <wandir@gmail.com>
2023-01-30 02:30:30 -0300

    Implementando as classes e interfaces para login

--- src/Api.Domain/Repository/IUserRepository.cs
+++ src/Api.Domain/Repository/IUserRepository.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interface;
 
 namespace Api.Domain.Repository
 {
-    public interface IUserRepository
+    public interface IUserRepository : IRepository<UserEntity>
     {
-        
+        Task<UserEntity?> FindByLogin(string email);
     }
+
 }
