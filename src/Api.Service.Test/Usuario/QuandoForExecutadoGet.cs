e47d6c1b6923978305644c6108ce9adb21921a44
WandirPereira <wandir@gmail.com>
2023-02-09 17:55:28 -0300

    Atualizando diagrams de classes

--- src/Api.Service.Test/Usuario/QuandoForExecutadoGet.cs
+++ src/Api.Service.Test/Usuario/QuandoForExecutadoGet.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
 using Api.Domain.Interface.Services.User;
 using Moq;
 
@@ -22,6 +23,20 @@ namespace Api.Service.Test.Usuario
             Assert.NotNull(result);
             Assert.True(result.Id == IdUsuario);
             Assert.Equal(NomeUsuario, result.Name);
+
+            // _serviceMock.Setup(m => m.Get(It.IsAny<Guid>)).Returns(Task.FromResult((UserDto) null);
+            // _service = _serviceMock.Object;
+            // var result = await _service.Get(IdUsuario);
+            // Assert.NotNull(result);
+            // Assert.True(result.Id == IdUsuario);
+            // Assert.Equal(NomeUsuario, result.Name);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Get(It.IsAny<Guid>())).Returns(Task.FromResult((UserDto)null));
+            _service = _serviceMock.Object;
+
+            var _record = await _service.Get(IdUsuario);
+            Assert.Null(_record);
         }
     }
 }
